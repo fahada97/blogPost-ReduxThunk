e14bd2f7d6f0526b50ed37d71dae479461040b9f
fahada97 <syedfahad.ahmed97@gmail.com>
2020-06-25 14:09:06 -0400

    initial commit, create action creators, and some components

---
+++ src/Actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceHolder from "../Apis/JSONplaceholder";
+
+export const fetchPosts = () => async dispatch =>{
+    const response = await jsonPlaceHolder.get("/posts");
+
+    dispatch({type: 'FETCH_POSTS', payload: response})
+};
